408ba1921a5f3fbbe402f6f27827917947027666
Serhii Manko <msa01051984@gmail.com>
2022-12-27 16:34:39 +0200

    update folder

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-import { makeRandomHexColor } from 'utils/colorFunction';
+import { makeRandomHexColor } from 'helpers/colorFunction';
 
 export const Statistics = ({ title, stats }) => {
   return (
